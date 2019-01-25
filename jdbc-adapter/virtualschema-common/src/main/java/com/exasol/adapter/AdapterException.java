dd1c572c81923d04142e92f43b32a375a3306936
Sebastian Bär <sebastian.baer@exasol.com>
2019-01-25 09:43:52 +0100

    #68: Added constructor that excepts the causing exception.

--- jdbc-adapter/virtualschema-common/src/main/java/com/exasol/adapter/AdapterException.java
+++ jdbc-adapter/virtualschema-common/src/main/java/com/exasol/adapter/AdapterException.java
@@ -1,8 +1,27 @@
 package com.exasol.adapter;
 
-public class AdapterException extends Exception{
+/**
+ * Virtual schema adapter exception.
+ */
+public class AdapterException extends Exception {
+    private static final long serialVersionUID = -5821711270758573195L;
 
-    public AdapterException(String message) {
+    /**
+     * Create an new {@link AdapterException}
+     *
+     * @param message error message
+     */
+    public AdapterException(final String message) {
         super(message);
+    }
+
+    /**
+     * Create an new {@link AdapterException}
+     *
+     * @param message error message
+     * @param cause   root cause
+     */
+    public AdapterException(final String message, final Exception cause) {
+        super(message, cause);
     }
 }
